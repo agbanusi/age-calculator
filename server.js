2e712fc441c6c25e9daab3bbd195bb99eb0fd5ca
John Agbanusi <agbanusijohn@gmail.com>
2022-08-10 14:48:05 +0100

    update api to use json format respose all through with message variable

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const ageLimiter = rateLimit({
   windowMs: 1000, // 1 second
   max: 3, // limit to 3 calls per second
-  message: "Too many calls, wait a few moments and try again",
+  message: { message: "Too many calls, wait a few moments and try again" },
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
@@ -28,14 +28,14 @@ app.get("/ageCalculator", ageLimiter, (req, res) => {
     const year = 1000 * 3600 * 24 * 365.25; //one year = 365 + 1/4 days
     const age = diff / year;
 
-    res.json({ age: Math.floor(age) });
+    res.json({ age: Math.floor(age), message: "successful" });
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
 });
 
 app.use("*", (req, res) => {
-  res.status(404).send("404 Not Found");
+  res.status(404).json({ message: "404 Not Found" });
 });
 
 app.listen(process.env.PORT, () => {
