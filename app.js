8b9c469b4f000afc0a1dcf18d52495728dbe114b
John Agbanusi <agbanusijohn@gmail.com>
2022-08-10 20:53:28 +0100

    Update error response object

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ const limiter = rateLimit({
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
-app.use(limiter);
 
-app.get("/howold", (req, res) => {
+app.get("/howold", limiter, (req, res) => {
   try {
     const { dob } = req.query;
     if (!dob || isNaN(dob)) {
-      return res.status(400).json({ message: "invalid query input" });
+      return res
+        .status(400)
+        .json({ message: "invalid query input", error: 400 });
     }
     const now = Date.now();
     const diff = now - dob;
@@ -24,19 +25,22 @@ app.get("/howold", (req, res) => {
     if (diff < 0) {
       return res
         .status(400)
-        .json({ message: "Invalid query input as age cannot be negative" });
+        .json({
+          message: "Invalid query input as age cannot be negative",
+          error: 400,
+        });
     }
     const year = 1000 * 3600 * 24 * 365.25; //one year = 365 + 1/4 days
     const age = diff / year;
 
     return res.json({ age: Math.floor(age), message: "successful" });
   } catch (e) {
-    return res.status(500).json({ message: e.message });
+    return res.status(500).json({ message: e.message, error: 500 });
   }
 });
 
 app.use("*", (req, res) => {
-  res.status(404).json({ message: "404 Not Found" });
+  res.status(404).json({ message: "404 Not Found", error: 404 });
 });
 
 module.exports = { app };
